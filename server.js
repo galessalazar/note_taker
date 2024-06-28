bc99cd33bdb90b43b64b9d9c20a3817603bb9339
Al Bundy <147283600+galessalazar@users.noreply.github.com>
2024-06-28 11:48:21 -0500

    added Html routes

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
+// On the back end, the application should include a db.json file that will be used to store and retrieve notes using the fs module.
+
+// need to require these to use
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const api = require('')
 
+// the process asks to use the assigned port, if not, use this one
 const PORT = process.env.PORT || 3001;
 
+// use the app variable when using express
 const app = express();
+
+// this will parse everything to json, the use is middleware
+app.use(express.json());
+
+
+// GET * should return the index.html file.
+app.get('*', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/assets/index.html')))
+
+
+// GET /notes should return the notes.html file.
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
+
+// The following API routes should be created:
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+app.get('/api/notes')
+);
