6dffa056b2675ba869269529feed88b1d0ee43e1
Al Bundy <147283600+galessalazar@users.noreply.github.com>
2024-06-30 12:30:54 -0500

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+// this may not work, its linking to this file so idk
 const notesRouter = require('./public/assets/js/index');
 
 router.use('/public/assets/index', notesRouter);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require('')
+const api = require('.routes/index.js');
 
 // the process asks to use the assigned port, if not, use this one
 const PORT = process.env.PORT || 3001;
@@ -19,16 +19,40 @@ app.use(express.json());
 
 // GET * should return the index.html file.
 app.get('*', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/assets/index.html')))
+res.sendFile(path.join(__dirname, '/public/assets/index.html'))
+);
 
 
 // GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
-
+);
 // The following API routes should be created:
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
-app.get('/api/notes')
-);
+// feedbackjs has an example of unique id
+
+// title and text
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+
+    const { title, text} = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+        };
+        append(newNote, './db/db.json');
+        res.json('New note added successfully');
+        } else {
+            res.error('note not added for some odd reason');
+        }
+    });
+
